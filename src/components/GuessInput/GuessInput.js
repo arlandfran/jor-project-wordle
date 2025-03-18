58c4a17548814fcffc3efd5052735d42c811c6fa
arlandfran <arland.torres@outlook.com>
2025-03-18 13:43:39 +0000

    Validate input text and log to console

--- src/components/GuessInput/GuessInput.js
+++ src/components/GuessInput/GuessInput.js
@@ -1,10 +1,25 @@
 import React from "react";
 
 function GuessInput() {
+  const [guess, setGuess] = React.useState("");
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    console.log(guess);
+    setGuess("");
+  }
+
   return (
-    <form className="guess-input-wrapper">
+    <form className="guess-input-wrapper" onSubmit={handleSubmit}>
       <label htmlFor="guess-input">Enter guess:</label>
-      <input id="guess-input" type="text" />
+      <input
+        id="guess-input"
+        type="text"
+        value={guess}
+        onChange={(event) => setGuess(event.target.value.toUpperCase())}
+        pattern="^.{5,5}$"
+        title="Must be five characters"
+      />
     </form>
   );
 }
